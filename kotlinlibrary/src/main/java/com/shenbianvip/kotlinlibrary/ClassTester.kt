245a68167a9db51e93b23410dbd8e98bbe6d40ea
yinhao <263667227@qq.com>
2017-08-30 17:45:39 +0800

    类测试代码

---
+++ kotlinlibrary/src/main/java/com/shenbianvip/kotlinlibrary/ClassTester.kt
@@ -0,0 +1,14 @@
+package com.shenbianvip.kotlinlibrary
+
+/**
+ * Created by admin on 2017/8/30.
+ */
+class ClassTester{
+
+    companion object {
+        fun main(args: Array<String>) {
+            println("test")
+        }
+    }
+
+}
